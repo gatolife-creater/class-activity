d866eee69b24e68c5964d42d65e42c44a444c5f2
gatolife-creater <gatolifechannel@gmail.com>
2022-01-09 08:11:47 +0900

    modified

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@ window.addEventListener("load", function() {
     whereAmI();
     setLang();
     detectJargon();
-    changePath();
 });
 
 window.addEventListener('scroll', function() {});
@@ -21,20 +20,6 @@ function setLang() {
     let lang = document.getElementsByTagName("html")[0].lang;
     lang = "jp";
     console.log(lang);
-}
-
-function changePath() {
-    if (window.location.href.includes("htt")) {
-        let scripts = document.getElementsByTagName("script");
-        for (let i = 0; i < scripts.length; i++) {
-            if (scripts[i].src.includes("sketch.js")) {
-                scripts[i].remove();
-                let alt_sketch = document.createElement("script");
-                alt_sketch.src = "/class-activity/js/sketch.js";
-                document.body.insertBefore(alt_sketch, document.body.firstChild);
-            }
-        }
-    }
 }
 
 let ku = ` ■■    ■                                                       
